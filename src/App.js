51cb976b253adade505c0c5da5f06a372b566ed2
woorix2 <woori8806@naver.com>
2022-05-27 00:07:00 +0900

    Feat: 유저 리스트 AddUser컴포넌트에서 입력받은 유저리스트 출력할 UsersList 컴포넌트 생성후 로직 작성, 스타일링

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
 function App() {
   return (
     <div className="App">
       <AddUser />
+      <UsersList users={[]} />
     </div>
   );
 }
---
+++ src/components/Users/UsersList.js
@@ -0,0 +1,17 @@
+import Card from "../UI/Card";
+import styles from "./UsersList.module.css";
+
+const UsersList = (props) => {
+  return (
+    <Card className={styles.users}>
+      <ul>
+        {props.users.map((user) => (
+          <li>
+            {user.name} ({user.age} years old)
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
+};
+export default UsersList;
