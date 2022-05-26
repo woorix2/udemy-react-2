39f03ed9a4c82f289412796d325c3cba8a009699
woorix2 <woorix2@naver.com>
2022-05-26 22:18:19 +0900

    Create: Card.js 재사용 가능한 Card 컴포넌트 만들어 Form태그를 감싸는 div태그를 대신함

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,16 +1,21 @@
+import Card from "../UI/Card";
+import styles from "./AddUser.module.css";
+
 const AddUser = (props) => {
   const addUserHandler = (e) => {
     e.preventDefault();
   };
-
+  console.log(styles);
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text" />
-      <label htmlFor="age">Age (Years)</label>
-      <input id="age" type="number" />
-      <button type="submit">Add user</button>
-    </form>
+    <Card className={styles.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" />
+        <button type="submit">Add user</button>
+      </form>
+    </Card>
   );
 };
 export default AddUser;
